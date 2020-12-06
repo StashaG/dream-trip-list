5a90b38f674ad4a98c73deea46bf75bb09177ec2
Stacey Graham <stacey.graham1106@gmail.com>
2020-12-06 16:10:48 -0500

    added Table Header and Table Body function component to the Table Component

--- src/Table.jsx
+++ src/Table.jsx
@@ -1,36 +1,49 @@
 import React, { Component } from 'react'
 
-class Table extends Component {
-    render() {
-        return (
-        <table class="table table-hover">
-            <thead>
-                <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Name</th>
-                    <th scope="col">Dream Trip Destination</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <th scope="row">1</th>
-                    <td>Stacey</td>
-                    <td>Kenya</td>
-                </tr>
-                <tr>
-                    <th scope="row">2</th>
-                    <td>Summer</td>
-                    <td>London</td>
-                </tr>
-                <tr>
-                    <th scope="row">3</th>
-                    <td colspan="1">Jasmine</td>
-                    <td>Thailand</td>
-                </tr>
-            </tbody>
-        </table>
-        )
-    }
-}
+const TableHeader = () => {
+    return (
+      <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Name</th>
+            <th scope="col">Dream Trip Destination</th>
+        </tr>
+      </thead>
+    )
+  }
+  
+  const TableBody = () => {
+    return (
+      <tbody>
+        <tr>
+            <th scope="row">1</th>
+            <td>Stacey</td>
+            <td>Kenya</td>
+        </tr>
+        <tr>
+            <th scope="row">2</th>
+            <td>Summer</td>
+            <td>London</td>
+        </tr>
+        <tr>
+            <th scope="row">3</th>
+            <td colspan="1">Jasmine</td>
+            <td>Thailand</td>
+        </tr>
+        </tbody>
+          )
+      }
+  
+  class Table extends Component {
+      render() {
+          return (
+          <table class="table table-hover">
+              <TableHeader />
+              <TableBody />
+          </table>
+          )
+      }
+  }
+  
 
 export default Table;
