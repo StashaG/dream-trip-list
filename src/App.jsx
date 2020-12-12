ad7b91c76db1d87020a6b3996f23959eba7d9f1c
Stacey Graham <stacey.graham1106@gmail.com>
2020-12-11 22:37:59 -0500

    adding the Form component to render below the Table component

--- src/App.jsx
+++ src/App.jsx
@@ -25,6 +25,7 @@ class App extends Component {
       return (
         <div className="container">
           <Table characterData={characters} removeCharacter={this.removeCharacter} />
+          <Form />
         </div>
       );
     }
